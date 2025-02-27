3ccfd72aecca320cbb06f3abc5987d70817007d3
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-02-27 15:10:35 +0200

    [dev][ci-jobs] Clean up the test env config options - remove unused cascade key (#55863)

--- tools/monorepo-utils/src/ci-jobs/lib/config.ts
+++ tools/monorepo-utils/src/ci-jobs/lib/config.ts
@@ -338,43 +338,9 @@ export interface TestJobConfig extends BaseJobConfig {
 	testEnv?: TestEnvConfig;
 
 	/**
-	 * The key(s) to use when identifying what jobs should be triggered by a cascade.
-	 */
-	cascadeKeys?: string[];
-
-	/**
 	 * The configuration for the report if one is needed.
 	 */
 	report?: ReportConfig;
-}
-
-/**
- * parses the cascade config.
- *
- * @param {string|string[]} raw The raw config to parse.
- */
-function parseTestCascade( raw: unknown ): string[] {
-	if ( typeof raw === 'string' ) {
-		return [ raw ];
-	}
-
-	if ( ! Array.isArray( raw ) ) {
-		throw new ConfigError(
-			'Cascade configuration must be a string or array of strings.'
-		);
-	}
-
-	const changes: string[] = [];
-	for ( const entry of raw ) {
-		if ( typeof entry !== 'string' ) {
-			throw new ConfigError(
-				'Cascade configuration must be a string or array of strings.'
-			);
-		}
-
-		changes.push( entry );
-	}
-	return changes;
 }
 
 /**
@@ -460,10 +426,6 @@ function parseTestJobConfig( raw: any ): TestJobConfig {
 			resultsPath: raw.report.resultsPath,
 			allure: raw.report.allure,
 		};
-	}
-
-	if ( raw.cascade ) {
-		config.cascadeKeys = parseTestCascade( raw.cascade );
 	}
 
 	return config;
